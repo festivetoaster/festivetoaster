0dec3de636b406d6340edf9c61f67bcf155250fb
Jackson <jsharf212@gmail.com>
2015-11-16 17:30:42 -0800

    changed data

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -102,7 +102,7 @@ module.exports = function (app, express) {
     res.send(JSON.stringify([
       {
         name: 'Jackson Sharf',
-        latest: 'Coding is fun',
+        latest: 'Tweet: \nCoding is fun',
         steps: 500,
         tweet: 'Coding is fun',
         gitCommit: 'Checking in some sweet code',
@@ -116,14 +116,14 @@ module.exports = function (app, express) {
         gitCommit: 'Checking in some mostly broken code',
         bookUrl: 'http://www.hubertuy.com/wp-content/uploads/2015/02/holy-bible-cover.jpg',
         book: 'The Bible',
-        latest: 'The Bible',
+        latest: 'Book: \nThe Bible',
         date: Date.now() + 1000},
       {
         name: 'Yoshio Goto',
         steps: 23694,
         tweet: 'Code Code Code',
         gitCommit: 'Checking in some slick front end updates',
-        book: 'Kafka on the Shore',
+        book: 'Book: \nKafka on the Shore',
         bookUrl: 'http://ecx.images-amazon.com/images/I/418ybjvpCPL._SY344_BO1,204,203,200_.jpg',
         latest: 'Kafka on the Shore',
         date: Date.now()}
